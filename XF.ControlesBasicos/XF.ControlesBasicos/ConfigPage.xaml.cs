b3da4e3bceb0e43d2d6c1c295a178c2fc6a6a013
Carlos Edurdo de Souza <kaka.nota10@hotmail.com>
2018-10-14 19:24:15 -0300

    Add project files.

---
+++ XF.ControlesBasicos/XF.ControlesBasicos/ConfigPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.ControlesBasicos
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ConfigPage : ContentPage
+	{
+		public ConfigPage ()
+		{
+            BindingContext = App.ConfigModel;
+            InitializeComponent ();
+		}
+
+        private void OnReceberEmail_Changed(object sender, EventArgs e)
+        {
+            var selecioneAtivo = ((SwitchCell)sender).BindingContext as Configuracao;
+
+
+            if (selecioneAtivo.ReceberEmail)
+                txtEnderecoEmail.IsVisible = true;
+            else
+                txtEnderecoEmail.IsVisible = false;
+        }
+    }
+}
---
+++ XF.ControlesBasicos/XF.ControlesBasicos/Configuracao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XF.ControlesBasicos
+{
+        public class Configuracao : INotifyPropertyChanged
+        {
+            public Configuracao()
+            {
+                receberEmail = false;
+                EnderecoEmail = "";
+            }
+
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            public void OnPropertyChanged()
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("EnderecoEmail"));
+            }
+
+            private bool receberEmail;
+            public bool ReceberEmail
+            {
+                get => receberEmail;
+                set
+                {
+                    if (receberEmail != value)
+                    {
+                        receberEmail = value;
+                        if (!receberEmail)
+                            EnderecoEmail = "";
+
+                        OnPropertyChanged();
+                    }
+                }
+            }
+            public string EnderecoEmail { get; set; }
+        }
+    }
